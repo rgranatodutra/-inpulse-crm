ef0731f153c9c0188858e22704bb9aa095518618
Renan G Dutra <r.granatodutra@gmail.com>
2024-07-10 21:18:25 -0300

    chore: export validate dto

--- utils/index.ts
+++ utils/index.ts
@@ -3,11 +3,13 @@ import toPaginated from "./src/toPaginated";
 import { FilterWithPaginationQueryParameters } from "./src/createWhereString.util";
 import getRouterEndpoints from "./src/getRouterEndpoints.util";
 import BasicCrud from "./src/basicCrud.class";
+import validateDto from "./src/validateDto";
 
 export {
     createWhereString,
     toPaginated,
     FilterWithPaginationQueryParameters,
     getRouterEndpoints,
-    BasicCrud
+    BasicCrud,
+    validateDto
 }
